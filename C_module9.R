6fb06606361cc8d9b07172bf6257b7d7cf07e6b0
Bri-LaCarubba <bjl1017@unh.edu>
2017-10-27 11:42:49 -0400

    Added a lot of comments and edited the function just to make it clearer.

--- C_module9.R
+++ C_module9.R
@@ -22,10 +22,10 @@ unique(msleep$vore)
 #briefly describe in 1-2 sentences the major patterns in the plot
 
 
-msleep2<-msleep[!is.na(msleep$vore),]
-unique(msleep2$vore)
+msleep2<-msleep[!is.na(msleep$vore),] ## removes NAs from sleep
+unique(msleep2$vore) ## what are the unique vore categories
 
-boxplot(msleep2$sleep_total ~ msleep2$vore, xlab= "Diet Type", ylab= "Daily Sleep Totals (hr)")
+boxplot(msleep2$sleep_total ~ msleep2$vore, xlab= "Diet Type", ylab= "Daily Sleep Totals (hr)") ## boxplot generation
 
 ## Insectivores sleep have the highest average daily sleep total (but also a lot of variation). 
 ## Carnivores, herbivores, and omnivores sleep (on average) for the same amount of time. 
@@ -34,21 +34,21 @@ boxplot(msleep2$sleep_total ~ msleep2$vore, xlab= "Diet Type", ylab= "Daily Slee
 ## Use plot() to show the relationship between the natural log of body size
 ## and the length of the sleep cycle 
 
-msleep3<-msleep[!is.na(msleep$sleep_cycle),]
-msleep3$logbodywt<-log(msleep3$bodywt)
+msleep3<-msleep[!is.na(msleep$sleep_cycle),] ## new subset removing NAs from sleep cycle
+msleep3$logbodywt<-log(msleep3$bodywt) ## add a column that is the log of bodyweight
 summary(msleep3)
 
 plot(msleep3$logbodywt, msleep3$sleep_cycle, xlab= "Log Body Weight (kg)", ylab = "Sleep Cycle Length (hr)", pch = 16, col= "blue")
 
 unique(msleep3$conservation)
-msleep4<-msleep3[!is.na(msleep3$conservation),]
+msleep4<-msleep3[!is.na(msleep3$conservation),] ## generate a fourth subset that removes NAs from conservation 
 summary(msleep4)
 unique(msleep4$conservation)
 
 ggplot(data = msleep4, aes(x=logbodywt, y=sleep_cycle))+
   geom_point()+
   facet_wrap(~ conservation)+
-  stat_smooth(method="lm", se=F)
+  stat_smooth(method="lm", se=F) ### plot with trend lines
 
 ## Yes, the species within each conservation group show a linear trend where 
 ## they increase the hours of daily sleep as they increase in body weight. This may not 
@@ -62,28 +62,31 @@ ggplot(data = msleep4, aes(x=logbodywt, y=sleep_cycle))+
 ## and three columns named "vore" "brain_body_mean" and "brain_body_se" 
 
 
-brain_body_ratio<- function(x,y,z){
+brain_body_ratio<- function(x,y,z){ ### x will be vore category, y will be brainwt, z will be bodywt
   
-  names<-as.data.frame(sort(unique(x)))
-  brain<-tapply(y, x, mean, na.rm=T)
-  body<-tapply(z, x, mean, na.rm=T)
+  names<-as.data.frame(sort(unique(x))) ### generate data frome of the vore names
+  brain<-tapply(y, x, mean, na.rm=T) ### mean of brains by vore category
+  body<-tapply(z, x, mean, na.rm=T) ### mean of bodywt by vore category
   
-  m<-brain/body
+  m<-brain/body  ## ratio of mean brain by body weight per vore category
   
-  brainsd<-tapply(y, x, sd, na.rm=T)
-  bodysd<-tapply(z, x, sd, na.rm=T)
-  brainse<-brainsd/sqrt(length(na.omit(brainsd)))
-  bodyse<-bodysd/sqrt(length(na.omit(bodysd)))
-  se<-brainse/bodyse
+  brainsd<-tapply(y, x, sd, na.rm=T) ## standard deviation of brainwt
+  bodysd<-tapply(z, x, sd, na.rm=T) ## standard deviation of bodywt
+  brainse<-brainsd/sqrt(length(na.omit(brainsd))) ## standard error of brainwt
+  bodyse<-bodysd/sqrt(length(na.omit(bodysd)))  ## standard error of bodywt
+  se<-brainse/bodyse #### standard error of ratio of brain to body 
   
-  s1<-cbind(names,m,se)
-  colnames(s1)<-c("vore","brain_body_mean","brain_body_se")
-  rownames(s1)<-NULL
-  return(s1)
+  s1<-cbind(names,m,se) ## generate a dataframe of all these categories binding together
+  colnames(s1)<-c("vore","brain_body_mean","brain_body_se") ## renames the columns 
+  rownames(s1)<-NULL ## rid of rownames
+  return(s1) ## return the dataframe from the function
 }
 
-test1 <- brain_body_ratio(msleep4$vore, msleep4$brainwt, msleep4$bodywt)
+test1 <- brain_body_ratio(msleep4$vore, msleep4$brainwt, msleep4$bodywt) ## test to see if function worked
 head(test1)
+
+tapply(msleep4$brainwt, msleep4$vore, mean, na.rm = T)/tapply(msleep4$bodywt,msleep4$vore, mean, na.rm = T) 
+### test to see if answers from function match up to these means. they do. function worked.
 
 msleep<-msleep[!is.na(msleep$vore),]
 msleep<-msleep[!is.na(msleep$brainwt),]
